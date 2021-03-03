0c93726f4e5abf8c3bfe4114b1a66447b9b02f93
Dilshodbeka <abdullayev.dima7080@gmail.com>
2021-03-03 20:47:08 +0100

    stopped where adding validator

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { IdeaDTO } from './idea.tdo';
@@ -24,15 +24,28 @@ export class IdeaService {
     }
 
     async read(id: string){
-        return await this.ideaRepository.findOne({where: {id}});
+        const idea = await this.ideaRepository.findOne({where: {id}});
+        if(!idea){
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
+        return idea;
     }
 
     async update(id: string, data: Partial<IdeaDTO>) {
-        await this.ideaRepository.update({id}, data);
-        return await this.ideaRepository.findOne({id});
+        const idea = await this.ideaRepository.findOne({where: {id}});
+        if(!idea){
+            throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
+        }
+        await this.ideaRepository.update({id}, data)
+        return idea;
     }
     async destroy(id: string){
-        await this.ideaRepository.delete(id);
-        return {deleted: true};
+       
+        const idea = await this.ideaRepository.findOne({where: {id}});
+        if(!idea){
+            throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
+        }
+        await this.ideaRepository.delete({id});
+        return idea;
     }
 }
